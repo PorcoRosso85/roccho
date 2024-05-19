1f4374c8ff16b1f859ef29be8e997bad453f33de
PorcoRosso85 <1.is.universe@gmail.com>
2024-05-19 19:17:24 +0900

    javascript

--- javascript_http/flake.nix
+++ javascript_http/flake.nix
@@ -3,15 +3,22 @@
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   inputs.flake-utils.url = "github:numtide/flake-utils";
+  inputs.subflake.url = "path:./sub_javascript_http";
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        defaultPackage = pkgs.writeShellScriptBin "run-server" ''
-          ${pkgs.nodejs}/bin/node ${./server.js}
-        '';
-      }
-    );
+  outputs = { self, nixpkgs, flake-utils, subflake }:
+    flake-utils.lib.eachDefaultSystem
+      (system:
+        let
+          pkgs = nixpkgs.legacyPackages.${system};
+          subflakePackage = subflake.packages.${system}.default;
+        in
+        {
+          defaultPackage = pkgs.writeShellScriptBin "run-server" ''
+            ${pkgs.nodejs}/bin/node ${./server.js}
+            ${subflakePackage}/bin/hello
+          '';
+
+          packages.subflake = subflakePackage;
+        }
+      );
 }
