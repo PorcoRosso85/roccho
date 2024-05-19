761bf1a90e23075c24b469d492e5ebd41d8f98dd
PorcoRosso85 <1.is.universe@gmail.com>
2024-05-19 16:29:14 +0900

    add javascript server

---
+++ javascript_http/flake.nix
@@ -0,0 +1,17 @@
+{
+  description = "Hello World HTTP server";
+
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let 
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        defaultPackage = pkgs.writeShellScriptBin "run-server" ''
+          ${pkgs.nodejs}/bin/node ${./server.js}
+        '';
+      }
+    );
+}
