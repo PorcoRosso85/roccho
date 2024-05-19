c65d49176e5540be49bd5663891a19852d20991f
PorcoRosso85 <1.is.universe@gmail.com>
2024-05-19 19:18:40 +0900

    python update

--- python_http/flake.nix
+++ python_http/flake.nix
@@ -8,18 +8,19 @@
 
   outputs = { self, nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
-      let 
-        pkgs = nixpkgs.legacyPackages.${system};
-      in {
-        apps.default = {
-          type = "app";
-          program = let
-            # Define the app script
-            app = pkgs.writeShellScriptBin "myapp" ''
-              echo "Starting app on localhost:8000"
-              ${pkgs.python3}/bin/python -m http.server 8000
-            '';
-          in "${app}/bin/myapp";
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in
+      {
+        defaultPackage = pkgs.mkShell {
+          buildInputs = [ pkgs.python3 pkgs.python3Packages.flask ];
+          shellHook = ''
+            echo "Hello, {input} + world"
+            export FLASK_APP=app.py
+            flask run
+          '';
         };
       }
     );
