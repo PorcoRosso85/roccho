1a18766fe48ec1ffb1ee4979ac0eeb4d1da62f47
PorcoRosso85 <1.is.universe@gmail.com>
2024-05-19 17:19:52 +0900

    hello, 複数システム対応を簡潔に定義

--- javascript_http/sub_javascript_http/flake.nix
+++ javascript_http/sub_javascript_http/flake.nix
@@ -1,15 +1,18 @@
 {
   description = "A simple flake";
-
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; {
-      hello = hello;
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      packages = forAllSystems (system: {
+        hello = nixpkgs.legacyPackages.${system}.hello;
+      });
+      defaultPackage = forAllSystems (system:
+        nixpkgs.legacyPackages.${system}.hello
+      );
     };
 
-    defaultPackage.x86_64-linux = self.packages.x86_64-linux.hello;
-
-  };
 }
