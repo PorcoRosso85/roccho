6fb6bd71f1d18c216ff4e4232f569f6e941de77c
PorcoRosso85 <1.is.universe@gmail.com>
2024-05-26 05:21:43 +0900

    not run multiple run command

--- lib/workers/flake.nix
+++ lib/workers/flake.nix
@@ -16,15 +16,39 @@
               };
 
           run = { pkgs }:
-            pkgs.writeShellScriptBin "hello" ''
-              echo "Hello, world!"
-              pnpm wrangler dev ./auth/src/index.ts
+            pkgs.writeShellScriptBin "run" ''
+              echo "nix run testing"
             '';
+
+          hello = { pkgs }:
+            pkgs.hello;
 
         in
         {
           devShell = develop { inherit pkgs; };
-          defaultPackage = run { inherit pkgs; };
+          # defaultPackage = run { inherit pkgs; };
+
+          packages.x86_64-linux = {
+            # hello = pkgs.hello;
+            hello = hello { inherit pkgs; };
+            cowsay = pkgs.cowsay;
+          };
+
+          apps.x86_64-linux = {
+            hi = { pkgs, }:
+              pkgs.writeShellScriptBin "hello" ''
+                echo "Hello World"
+              '';
+            hello = {
+              type = "app";
+              program = "${self.packages.x86_64-linux.hello}/bin/hello";
+            };
+
+            cowsay = {
+              type = "app";
+              program = "${self.packages.x86_64-linux.cowsay}/bin/cowsay";
+            };
+          };
         }
       );
 }
